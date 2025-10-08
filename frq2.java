4ff3baa52dcd842b46c4f364a9c959ac37baba69
howieju <howieju@gmail.com>
2025-10-08 12:23:27 -0700

    Update frq2.java

--- frq2.java
+++ frq2.java
@@ -2,7 +2,7 @@ public class frq2 {
     public static void main(String[] args) {
         int rows = 6;
         int cols = 8;
-
+        //// missing validator for 0 and less. -2
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
                 System.out.print("Q "); 
