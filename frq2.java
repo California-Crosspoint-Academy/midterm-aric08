bd66067cf009b8e9a56f4876d385c4b3e59934d9
aric08 <aricmar26@crosspointacademy.org>
2025-10-08 16:45:06 +0000

    frq mid term

---
+++ frq2.java
@@ -0,0 +1,13 @@
+public class frq2 {
+    public static void main(String[] args) {
+        int rows = 6;
+        int cols = 8;
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print("Q "); 
+            }
+            System.out.println();  
+        }
+    }
+}
